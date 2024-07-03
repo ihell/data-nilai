defc46cb0e6f5ea1e1b440452ff0e7142f3eee97
ihell <ihell269@gmail.com>
2024-07-03 19:11:35 +0700

    memperbaikin urutan code

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 mahasiswa = {
     "G.1" : ["Ryo", 97],
-    "G,3" : ["Nijika", 90],
-    "G,2": ["Kita", 89]
+    "G.3" : ["Nijika", 90],
+    "G.2": ["Kita", 89]
 }
 
 print("=======Data Nilai Mahasiswa=======")
@@ -16,7 +16,7 @@ code_sorting = {
 }
 print("=====Pilih Metode Pengurutan Data=====")
 for i in code_sorting:
-    print("Code Sorting : ", i, "---", "Metode SOrting : ", code_sorting[i])
+    print("Code Sorting : ", i, "---", "Metode Sorting : ", code_sorting[i])
 
 sorting = input("Masukan Code Sorting : ")
 if sorting == "1":
@@ -24,4 +24,18 @@ if sorting == "1":
     print("=======Data Nilai Mahasiswa Berdasarkan NIM=======")
     for n in sorted_mahasiswa:
         print("Nama : ", sorted_mahasiswa[n][0],"\t",
-          "NIM : ", n, "\t", "Nilai : ", sorted_mahasiswa[n][1]) 
+              "NIM : ", n, "\t", "Nilai : ", sorted_mahasiswa[n][1])
+elif sorting == "2":
+    sorted_mahasiswa = {k:v for k, v in sorted(mahasiswa.items(), key=lambda item: item[1][0])}
+    print("=======Data Nilai Mahasiswa Berdasarkan Nama=======")
+    for n in sorted_mahasiswa:
+        print("Nama : ", sorted_mahasiswa[n][0],"\t",
+              "NIM : ", n, "\t", "Nilai : ", sorted_mahasiswa[n][1])
+elif sorting == "3":
+    sorted_mahasiswa = {k:v for k, v in sorted(mahasiswa.items(), key=lambda item: item[1][1], reverse=True)}
+    print("=======Data Nilai Mahasiswa Berdasarkan Nilai Tertinggi=======")
+    for n in sorted_mahasiswa:
+        print("Nama : ", sorted_mahasiswa[n][0],"\t",
+              "NIM : ", n, "\t", "Nilai : ", sorted_mahasiswa[n][1])  
+else:
+    print("Kode sorting tidak valid")
